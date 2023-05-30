da9c8aa933dab26b8ed86d17dc7345fdd708e62c
Shanks <10147809+warshanks@users.noreply.github.com>
2023-05-29 23:01:54 -0500

    Delete install.sh

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-wget https://raw.githubusercontent.com/warshanks/funkydrac/main/funkydrac.zsh-theme
-wget https://raw.githubusercontent.com/warshanks/funkydrac/main/funkydrac2.zsh-theme
-mv funkydrac.zsh-theme ~/.oh-my-zsh/themes/
-mv funkydrac2.zsh-theme ~/.oh-my-zsh/themes/
-echo "Success\!"
